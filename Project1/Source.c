86f98a3e0755a6b7c4565f5f0bb76a2939f0f766
spanglies <eedqsa@gmail.com>
2018-12-04 03:24:37 -0800

    starting

--- Project1/Source.c
+++ Project1/Source.c
@@ -2,6 +2,22 @@
 
 #define PAGE_SIZE 0xFF
 #define MEM_PAGES 0xFF
+#define TLB_SIZE 0x0F
+#define pagedt char
 
-char mem[PAGE_SIZE][MEM_PAGES];
+char mem[MEM_PAGES][PAGE_SIZE];
+pagedt pageLookUp[MEM_PAGES];
+char  pageLookUpValid[MEM_PAGES];
+pagedt TLB[TLB_SIZE];
 
+void main(int argc, char **argv)
+{
+	FILE * addresses,  * bin;
+	int 
+	addresses = fopen("addresses.txt", "R");
+	bin = fopen("BACKING_STORE.bin", "RB");
+
+
+
+
+}
